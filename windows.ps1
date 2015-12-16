8251b5b51106bc6c181857c6b670b3e5ef8b9931
Mitchel Cabuloy <mixxorz@gmail.com>
2015-12-17 00:11:48 +0800

    Fix indentation on windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,94 +1,95 @@
 # Spacesuit
 # Turn your computer into an awesome software development machine!
 
+function Install-Chocolatey {
+    [CmdletBinding()]
+    param()
+    process {
+        if ((Get-Command "choco.exe" -ErrorAction SilentlyContinue) -eq $null) {
+            Write-Host "Installing Chocolatey"
+            iex ((New-Object Net.WebClient).DownloadString("http://chocolatey.org/install.ps1"))
+        } else {
+            Write-Host "Chocolatey already installed. Skipping ..."
+        }
+    }
+}
 
 function Get-InstalledPackages {
-	[CmdletBinding()]
-	[OutputType([array])]
-	param()
-	process {
-		$output = clist -lr
-		$results = $output.Split("[`r`n]")
-		$packages = New-Object System.Collections.ArrayList
-		ForEach ($result in $results) {
-			$packages.Add($result.Split("|")[0]) | Out-Null
-		}
-		$packages
-
-	}
+    [CmdletBinding()]
+    [OutputType([array])]
+    param()
+    process {
+        $output = clist -lr
+        $results = $output.Split("[`r`n]")
+        $packages = New-Object System.Collections.ArrayList
+        ForEach ($result in $results) {
+            $packages.Add($result.Split("|")[0]) | Out-Null
+        }
+        $packages
+    }
 }
 
 function Test-Installed {
-	[CmdletBinding()]
-	[OutputType([bool])]
-	param(
-		[Parameter(Mandatory=$true,
-			   ValueFromPipelineByPropertyName=$true,
-			   Position=0)]
-		[string]
-		$Package
-	)
-	process {
-		$result = Get-InstalledPackages
-		$result -contains $Package
-	}
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string]
+        $Package
+    )
+    process {
+        $result = Get-InstalledPackages
+        $result -contains $Package
+    }
 }
 
 function Test-Upgradable {
-	[CmdletBinding()]
-	[OutputType([bool])]
-	param(
-		[Parameter(Mandatory=$true,
-			   ValueFromPipelineByPropertyName=$true,
-			   Position=0)]
-		[string]
-		$Package
-	)
-	process {
-		$output = cup $Package -whatif -r
-		$upgradable = $output.Split("|")[-1]
-		[System.Convert]::ToBoolean($upgradable)
-	}
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param(
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string]
+        $Package
+    )
+    process {
+        $output = cup $Package -whatif -r
+        $upgradable = $output.Split("|")[-1]
+        [System.Convert]::ToBoolean($upgradable)
+    }
 }
 
 function Sync-Package {
-	[CmdletBinding()]
-	param(
-		[Parameter(Mandatory=$true,
-			   ValueFromPipelineByPropertyName=$true,
-			   Position=0)]
-		[string]
-		$Package
-	)
-	process {
-		if (Test-Installed $Package) {
-			if (Test-Upgradable $Package) {
-				Write-Host "Upgrading" $Package "..."
-				cup $Package -y
-			} else {
-				Write-Host "Already using the latest version of $Package. Skipping ..."
-			}
-		} else {
-			Write-Host "Installing $package ..."
-			cinst $Package -y
-		}
-	}
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string]
+        $Package
+    )
+    process {
+        if (Test-Installed $Package) {
+            if (Test-Upgradable $Package) {
+                Write-Host "Upgrading" $Package "..."
+                cup $Package -y
+            } else {
+                Write-Host "Already using the latest version of $Package. Skipping ..."
+            }
+        } else {
+            Write-Host "Installing $package ..."
+            cinst $Package -y
+        }
+    }
 }
 
-function Install-Chocolatey {
-	[CmdletBinding()]
-	param()
-	process {
-		if ((Get-Command "choco.exe" -ErrorAction SilentlyContinue) -eq $null) {
-			Write-Host "Installing Chocolatey"
-			iex ((New-Object Net.WebClient).DownloadString("http://chocolatey.org/install.ps1"))
-		} else {
-			Write-Host "Chocolatey already installed. Skipping ..."
-		}
-	}
-}
 
 Install-Chocolatey
+
+# Packages
 Sync-Package "sudo"
 Sync-Package "slack"
 Sync-Package "sublimetext3"
